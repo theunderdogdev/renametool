5baf19fae185b60481b639f70118fd1dbd0a3b66
RUTURAJ KALSHETTI <65947291+anonymousCoder04@users.noreply.github.com>
2021-06-05 17:00:02 +0530

    Update renameTool.py
    Added Auto Rename files

--- renameTool.py
+++ renameTool.py
@@ -4,6 +4,7 @@ import platform
 from tkinter import filedialog, messagebox
 import tkinter as tk
 import atexit
+import re
 
 
 class RenameTool:
@@ -22,14 +23,14 @@ class RenameTool:
 
         # Labels
         self.rename_label = tk.Label(text="New Name\n(w/ ext):", bg='black', fg='white', font=('Calibri', '12'))
-        self.rename_label.place(x=5, y=440)
+        self.rename_label.place(x=5, y=465)
         self.label = tk.Label(text="Enter Path:", height=1, bg='black', fg='white', font=('Calibri', '15'))
         self.label.place(x=5, y=5)
         self.label2 = tk.Label(text="Extension:", height=1, bg='black', fg='white', font=('Calibri', '15'))
         self.label2.place(x=5, y=65)
 
         # Output To List All files from the directory
-        self.output = tk.Text(width=75, height=15, padx=10, pady=5, font='Calibri', fg='green') # noqa
+        self.output = tk.Text(width=75, height=15, padx=10, pady=5, font='Calibri', fg='green')  # noqa
         self.output.place(x=115, y=145)
         self.output.bind("<1>", lambda event: self.output.focus_set())
 
@@ -38,34 +39,37 @@ class RenameTool:
         self.entry_1.place(x=115, y=5)
         self.entry_2 = tk.Text(width=10, height=1, padx=5, pady=10, font='Calibri')
         self.entry_2.place(x=115, y=65)
-        self.entry_3 = tk.Text(width=75, height=1, padx=5, pady=5, font='Calibri')
-        self.entry_3.place(x=115, y=450)
+        self.entry_3 = tk.Text(width=75, height=1, padx=5, pady=10, font='Calibri')
+        self.entry_3.place(x=115, y=470)
         # Control Buttons
         self.button = tk.Button(text="Submit", height=2, width=10, command=self.print_widget)
         self.button.place(x=230, y=65)
         self.rename = tk.Button(text="Rename", height=2, width=10, command=self.rename)
-        self.rename.place(x=115, y=500)
+        self.rename.place(x=740, y=470)
+        self.a_rename = tk.Button(text="Auto Rename", height=2, width=12, command=self.auto_rename)
+        self.a_rename.place(x=835, y=470)
         self.get_dir_button = tk.Button(text="Open", height=2, width=10, command=self.select_path)
         self.get_dir_button.place(x=325, y=65)
 
     def provide_option(self, files):
         if platform.system() == "Windows":
-            self.opt_menu = tk.Listbox(self.win, width=65, height=10, font='Calibri 12')
+            self.opt_menu = tk.Listbox(self.win, width=75, height=10, font='Calibri 12')
         else:
-            self.opt_menu = tk.Listbox(self.win, width=65, height=10, font='Calibri 16')
+            self.opt_menu = tk.Listbox(self.win, width=75, height=10, font='Calibri 16')
         for file in files:
-            if file != 'changes.log':
+            if file != 'changes.log' and not file.endswith('.BIN') and not file.endswith(
+                    '.Msi') and not file.startswith('.'):
                 self.opt_menu.insert('end', f'  {file}')
-        self.opt_menu.place(x=225, y=500)
+        self.opt_menu.place(x=115, y=525)
 
     def clear_prev(self):
         self.opt_menu.place_forget()
 
-
-    def select_path(self): # noqa
+    def select_path(self):  # noqa
         self.directory = filedialog.askdirectory()
 
     def scan_dir(self, path, extension):
+        extension = extension.replace(".", "") if "." in extension else extension
         try:
             if extension == "":
                 return os.listdir(path)
@@ -118,12 +122,14 @@ class RenameTool:
         self.output.delete(1.0, tk.END)
         print(dir_path)
         if dir_op is not None and len(dir_op) != 0:
+            print('Executed')
             self.provide_option(dir_op)
             for tex in dir_op:
-                if tex != 'changes.log':
-                    self.output.insert(tk.END, tex+'\n')
-            self.output.config(state="disabled")
-            self.refresh_options()
+                if tex != 'changes.log' and not tex.endswith('.BIN') and not tex.endswith('.Msi'
+                                                                                          ) and not tex.startswith('.'):
+                    self.output.insert(tk.END, tex + '\n')
+        self.refresh_options()
+        self.output.config(state="disabled")
 
     def refresh_options(self):
         dir_op = self.scan_dir(self.root_path[-1], self.entry_2.get(1.0, tk.END).replace("\n", ""))
@@ -134,7 +140,53 @@ class RenameTool:
             mode = 'a' if os.path.exists(self.root_path[-1] + '/changes.log') else 'w'
             log_file = open(self.root_path[-1] + '/changes.log', mode)
             for key, value in self.changes.items():
-                log_file.write(f'{key} --> {value}\n')
+                log_file.write(f'{key.encode("ascii", "replace").decode()} --> {value}\n')
+            log_file.close()
+
+    def auto_rename(self):
+
+        consent = messagebox.askyesno('Auto Renaming!',
+                                      "The ill-named files are going to be renamed automatically..Continue?")
+        if consent:
+            pw_dir = self.root_path[-1] + '/'
+            validchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_-().abcdefghijklmnopqrstuvwxyz01234567890"  # noqa
+            invalid_begin = "_-()."
+            for file in os.listdir(pw_dir):
+                try:
+                    if not os.path.isdir(pw_dir + f'/{file}') and not file.endswith('.BIN') and not file.endswith(
+                            '.Msi') and not file.startswith('.') and not file == 'changes.log' and pw_dir != "C:/":
+                        mod_file, ext = "", ""
+                        if file.count('.') > 1:
+                            mod_file = file[:file.rfind('.')].replace(".", '-')
+                            ext = file[file.rfind('.'):]
+                        else:
+                            mod_file = file
+                        if ('(' in mod_file or ')' in file or '()' in mod_file) and re.search('(\([0-9]\))',
+                                                                                              mod_file) is None:  # noqa
+                            mod_file = mod_file.replace(')', "").replace('(', "")
+                        while True:
+                            if mod_file[0] in invalid_begin:
+                                mod_file = mod_file[1:]
+                            else:
+                                break
+
+                        new_name = ""
+                        mod_file = mod_file.replace(" ", "-")
+                        for name in mod_file:
+                            if name in validchars:
+                                new_name += name
+                        if "__" in new_name:
+                            new_name = new_name.replace("__", "")
+
+                        if "--" in new_name:
+                            new_name = new_name.replace("--", "-")
+
+                        new_name = new_name + ext
+                        if file != new_name:
+                            self.changes[file] = new_name
+                        os.rename(pw_dir + file, pw_dir + new_name)
+                except (PermissionError, FileExistsError):
+                    print("File not changed")
 
     def add_widgets(self):
         self.win.mainloop()
